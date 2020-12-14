3eacb46acdf8bde1cc502f6c64bb6751564bbdf2
angelokeirsebilck <angelo.keirsebilck1@telenet.be>
2020-12-14 14:43:57 +0100

    Deploy settings

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -8,13 +9,11 @@ connectDB();
 
 // Init Middlewarde
 
-app.use(express.json({
-    extended: false
-}));
-
-app.get('/', (req,res) => {
-    res.send('API running');
-});
+app.use(
+  express.json({
+    extended: false,
+  })
+);
 
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
@@ -22,8 +21,18 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/profile', require('./routes/api/profile'));
 
+// Serve static adssets in production
+if (process.env.NODE_ENV == 'producten') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Server up and running on port ${PORT}`);
+  console.log(`Server up and running on port ${PORT}`);
 });
